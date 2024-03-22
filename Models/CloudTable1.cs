5c24eed8bba0e59089d01e1313566630211bbf8c
mathewsthania <ST10381071@VCCONNECT.EDU.ZA>
2024-03-22 13:22:29 +0200

    Add project files.

---
+++ Models/CloudTable1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace CLDV_ASSIGNMENT.Models
+{
+	public class CloudTable1 : Controller
+	{
+		public static string con_string = "Server=tcp:sql-databaset-utorial.database.windows.net,1433;Initial Catalog=SQLDatabase;Persist Security Info=False;User ID=thaniamathews;Password=SummerMe26;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
+		public static SqlConnection con = new SqlConnection(con_string);
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
